f85ac9dcf865cbbf6488a34d2fcd56c2e000af16
Hualdo <hucastillo@gmail.com>
2022-06-02 09:48:05 -0500

    Reply Amy Mobile version

--- comments.js
+++ comments.js
@@ -49,16 +49,86 @@ xmlhttp.onload = function(){
     document.getElementById('ransesPhoto').src=ransesPhoto
     document.getElementById('ransesName').textContent=ransesName
     document.getElementById('ransesTime').textContent=ransesDate
-    document.getElementById('ransesComment').textContent=ransesComment
+    document.getElementById('ransesComment').textContent='@maxblagun '+ ransesComment
     document.getElementById('ransesNumber').textContent=ransesScore
 
     //Julius Comment
     document.getElementById('juliusPhoto').src=juliusPhoto
     document.getElementById('juliusName').textContent=juliusName
     document.getElementById('juliusTime').textContent=juliusDate
-    document.getElementById('juliusComment').textContent=juliusComment
+    document.getElementById('juliusComment').textContent='@ransesmiron '+juliusComment
     document.getElementById('juliusNumber').textContent=juliusScore
     document.getElementById('juliusPhotoR').src=juliusPhoto
+
+    //Plus and Minus Function
+    let amyPlus = document.getElementById('amyPlus')
+    let amyMinus = document.getElementById('amyMinus')
+    let maxPlus = document.getElementById('maxPlus')
+    let maxminus = document.getElementById('maxMinus')
+    let ransesPlus = document.getElementById('ransesPlus')
+    let ransesMinus = document.getElementById('ransesMinus')
+    let juliusPlus = document.getElementById('juliusPlus')
+    let juliusMinus = document.getElementById('juliusMinus')
+    let amyNumber = document.getElementById('amyNumber').textContent
+    let maxNumber = document.getElementById('maxNumber').textContent
+    let ransesNumber = document .getElementById('ransesNumber').textContent
+
+    amyPlus.addEventListener('click',()=>{
+       amyNumber=parseInt(amyNumber)+1
+       document.getElementById('amyNumber').textContent=amyNumber
+       amyPlus.style.pointerEvents='none'       
+    })
+
+    amyMinus.addEventListener('click',()=>{
+        amyNumber=parseInt(amyNumber)-1
+        document.getElementById('amyNumber').textContent=amyNumber 
+        amyMinus.style.pointerEvents='none'       
+     })
+
+     maxPlus.addEventListener('click',()=>{
+        maxNumber=parseInt(maxNumber)+1
+        document.getElementById('maxNumber').textContent=maxNumber
+        maxPlus.style.pointerEvents='none'       
+     })
+
+     maxMinus.addEventListener('click',()=>{
+        maxNumber=parseInt(maxNumber)-1
+        document.getElementById('maxNumber').textContent=maxNumber 
+        maxMinus.style.pointerEvents='none'       
+     })
+
+     //Reply Function
+     let amyReply = document.getElementById('amyReply')
+     console.log(amyReply)
+     let number=0
+     amyReply.addEventListener('click',()=>{
+         number+=1
+         
+         let div = document.createElement('div')
+         div.className='commentReply'
+         div.id='amyCommentReply'
+         document.getElementById('amyComments').appendChild(div)
+
+         let textArea = document.createElement('textarea')
+         textArea.className='textArea'
+         textArea.textContent=`@${amyName}, `
+         document.getElementById('amyCommentReply').appendChild(textArea)    
+
+         let divAddMessage = document.createElement('div')
+         divAddMessage.className='add'
+         divAddMessage.id=`divAddMessage${number}`
+         document.getElementById('amyCommentReply').appendChild(divAddMessage)
+         console.log(divAddMessage)
+         let image =document.createElement('img')
+         image.src='./images/avatars/image-juliusomo.png'
+         console.log(image)
+         document.getElementById(`divAddMessage${number}`).appendChild(image)
+
+         let button = document.createElement('button')
+         button.textContent='send'
+         document.getElementById(`divAddMessage${number}`).appendChild(button)
+         
+     })
 
     
 }
